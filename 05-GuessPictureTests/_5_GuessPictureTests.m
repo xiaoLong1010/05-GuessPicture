ea312c17711c7aeefb8e8e39a0eb7559a8ab43fe
csy <csy@csys-Mac.local>
2014-05-23 09:22:27 +0800

    Initial Commit

---
+++ 05-GuessPictureTests/_5_GuessPictureTests.m
@@ -0,0 +1,34 @@
+//
+//  _5_GuessPictureTests.m
+//  05-GuessPictureTests
+//
+//  Created by csy on 5/23/14.
+//  Copyright (c) 2014 cl. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _5_GuessPictureTests : XCTestCase
+
+@end
+
+@implementation _5_GuessPictureTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
